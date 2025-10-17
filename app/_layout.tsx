4ff04baa374b5ec110bb0081b8086e29455d2145
Yeampier J. Huerta <yjhuerta@grupoctg.local>
2025-10-17 10:03:55 -0400

    feat:styles natiwind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,8 @@
+import { Slot } from "expo-router";
+import "../global.css";
 
-import React from "react";
-import { Text, View } from "react-native";
+const RootLayout = () => {
+  return <Slot></Slot>;
+};
 
-export default function RootLayout() {
-  return (
-     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-        <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
-}
+export default RootLayout;
